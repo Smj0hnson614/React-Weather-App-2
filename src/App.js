c786aff7a6aee8a423652dc99383f109fe09319a
Smj0hnson614 <153125119+Smj0hnson614@users.noreply.github.com>
2024-04-22 22:36:28 -0400

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Sabrina Johnson and is{" "}
-        <a
-          href="https://github.com/Smj0hnson614/React-Weather-App-2"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Sabrina Johnson and is{" "}
+          <a
+            href="https://github.com/Smj0hnson614/React-Weather-App-2"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
